30c825ff3a356756eb92b8f69afb7407532d5c57
Aitor Gallardo <38296940+AitorGallardo@users.noreply.github.com>
2023-05-08 23:48:19 +0200

    card styles done

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,23 @@ function App() {
   return (
     <>
       <div className='app__container'>
+        <div className='app__frontface_card-container'>
+          <div className='app__frontface_card-content'>
+            <span className='app__frontface_card-numbers'>
+              0000 0000 0000 0000
+            </span>
+            <div>
+              <span className='app__frontface_card-name'>JANE APPLESEED</span>
+              <span className='app__frontface_card-date'>00/00</span>
+            </div>
+          </div>
+        </div>
+        <div className='app__backface_card-container'>
+          <span className='app__backface_card-cvc'>000</span>
+        </div>
         <div className='app__container-background_left'></div>
         <div className='app__container-background_right'>
-          <Form/>
+          <Form />
         </div>
       </div>
     </>
